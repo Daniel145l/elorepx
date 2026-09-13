fb67d5a7e081cd59385dcd2c59e6bf84e1975de9
Daniel Lucas <daniellucasm18@gmail.com>
2026-09-13 00:14:48 -0300

    style: estilizando o header da ferramenta

---
+++ frontend/src/components/header/Header.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Menu } from 'lucide-vue-next';
+</script>
+
+<template>
+  <header class="flex justify-between items-center py-5 px-5 align-middle border-b shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]">
+    <RouterLink to="/">
+      <img src="/logo.svg" alt="Elorepx">
+    </RouterLink>
+    <Menu :size="20"/>
+  </header>
+</template>
